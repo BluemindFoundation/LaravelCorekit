e7810e452fb5c8da7878cbd35f08497f7273c023
AirCode <aircode.ds@gmail.com>
2025-07-20 01:52:06 +0000

    add Http service

--- src/Enums/OTPTypeEnum.php
+++ src/Enums/OTPTypeEnum.php
@@ -6,4 +6,13 @@ enum OTPTypeEnum: string
 {
     case AUTHENTICATION = 'authentication';
     case RESET_PASSWORD = 'reset-password';
+
+    public static function fromString(string $value): self
+    {
+        return match ($value) {
+            'authentication'   => self::AUTHENTICATION,
+            'reset-password'   => self::RESET_PASSWORD,
+            default => throw new \InvalidArgumentException("Invalid OTP type: $value"),
+        };
+    }
 }
