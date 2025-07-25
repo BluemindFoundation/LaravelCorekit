049111b2721bf773b7941b51f372f7a27fc4d1d1
AirCode <aircode.ds@gmail.com>
2025-07-25 21:40:26 +0000

    add tranlation services

--- src/Models/Translation.php
+++ src/Models/Translation.php
@@ -2,10 +2,11 @@
 
 namespace Corekit\Models;
 
-
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 
 class Translation extends Model
 {
+    use HasUlids;
     protected $fillable = ['table_name', 'column_name', 'row_id', 'locale', 'value',];
 }
